11ffabb160ea978fd594601122e13002e97d3c8a
Karol Mikołajczuk <karol.mikolajczuk@outlook.com>
2020-04-12 17:25:04 +0200

    Setting to upper first letters of title.

--- ModernCPP/ModernCPP/SetCapitalLetter.hpp
+++ ModernCPP/ModernCPP/SetCapitalLetter.hpp
@@ -5,10 +5,12 @@
 
 namespace CapitalLetter {
 
+	// when many one worded strings
 	std::string setCapitalLetters() { 
 		return "";
 	}
 
+	// when many one worded strings
 	template <typename TEXT, typename... Args>
 	std::string setCapitalLetters(TEXT text, Args... args) {
 		std::ostringstream oss_temp;
@@ -16,6 +18,21 @@ namespace CapitalLetter {
 			static_cast<std::string>(text).substr(1) << " ";
 	
 		return oss_temp.str() + setCapitalLetters(args...);
+	}
+
+	// when one string with many words
+	std::string setCapitalLettersInString(std::string text) {
+		
+		for (auto index = std::begin(text); index != std::end(text); ++index) {
+			if (index != std::begin(text)) {
+				if (std::isspace(*(index - 1))) *index = std::toupper(*index);
+			}
+			else {
+				if (!std::isspace(*index)) *index = std::toupper(*index);
+			}
+		}
+
+		return text;
 	}
 }
 
